e976a367b4f9d63c32ac8ecb34f8172ce9c9af41
Leonardo Tiago <pequenotiago@gmail.com>
2025-03-11 21:08:24 +0100

    Listagem fake dos Álbums

--- pages/albums.php
+++ pages/albums.php
@@ -1,1 +1,16 @@
-albuns
+<h1>Album</h1>
+
+<div class="row">
+    <?php
+        for ($i = 1; $i <= 10; $i ++):
+    ?>
+    <div class="col-3 album">
+        <a href="">
+            <img src="/albums/iza.jpeg" alt="<?=$i?>" class="img-album">
+            <h4>Album <?=$i?></h4>
+        </a>
+    </div>
+    <?php
+        endfor;
+    ?>
+</div>
